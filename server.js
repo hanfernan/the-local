0640e6f5b87b22969969c1a05a5535f175790445
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-05-20 15:40:47 -0500

    connected server

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
 // Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
