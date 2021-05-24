dc09c0680b920c19d943949005de21a8cd85635d
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-24 19:05:57 -0400

    updated app.js to route to navbar links

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from 'axios'
 import API from './utils/API'
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home'
+import Logout from './pages/Logout'
+import BandUser from './pages/BandUser'
+import Search from './pages/SearchResults'
 
 function App() {
-  useEffect( () => {
-    API.getBands().then(res => {console.log(res.data)})
+  useEffect(() => {
+    API.getBands().then(res => { console.log(res.data) })
   }, [])
   return (
     <div className="App">
-      <Navbar/>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/search" exact component={Search} />
+          <Route path="/band" exact component={BandUser} />
+          <Route path="/logout" exact component={Logout} />
+        </Switch>
+      </Router>
       {/* <h1>hello</h1> */}
     </div>
   );
