f5638c72a45f6a9a9404f1179ff5529a83037bd0
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-25 19:11:56 -0400

    started submit functionality. need to link to api route to finsih it.

--- client/src/components/Registration/Login.js
+++ client/src/components/Registration/Login.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Registration.css";
 
 function Login() {
+
+    const [info, setInfo] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // need login route for API request
+        //API.request(info)
+        console.log(info)
+    }
     return (
         <section className="reg-card-container">
             <h2 id="login">LOG IN</h2>
-            <form class="login-form">
+            <form class="login-form" onSubmit={handleSubmit}>
                 <h3>EMAIL</h3>
-                <input id="email-login" type="EMAIL" />
+                <input
+                    id="email-login"
+                    type="EMAIL"
+                    name="email"
+                    onChange={e => setInfo({ ...info, [e.target.name]: e.target.value })} />
+                {/* square brackets allow you to insert the key */}
                 <h3>PASSWORD</h3>
-                <input id="password-login" type="password" />
+                <input
+                    id="password-login"
+                    type="password"
+                    name="password"
+                    onChange={e => setInfo({ ...info, [e.target.name]: e.target.value })}
+                />
                 <div>
-                    <button class="login-form">LOGIN</button>
+                    <button class="login-form" type="submit">LOGIN</button>
                 </div>
             </form>
         </section>
