4189dc874531a50b8e0d06c964b2eafc36f3c226
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-05-25 13:38:34 -0500

    dynamically generated dta into search table

--- client/src/components/ResultsTable/ResultsTable.js
+++ client/src/components/ResultsTable/ResultsTable.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 function ResultsTable(props) {
+  const bands = props.results.map((band, i) => {
+    return (
+      <tr key={i}>
+        <td>{band.name}</td>
+        <td>{band.city}</td>
+        <td>{band.genre}</td>
+      </tr>
+    );
+  });
+
   return (
     <table className="table col-10 m-auto">
       <thead>
@@ -10,47 +20,9 @@ function ResultsTable(props) {
           <th scope="col">Genre</th>
         </tr>
       </thead>
-      {/* <tbody>
-        {props.bands &&
-          props.bands.length &&
-          props.bands.map((band, i) => (
-            <Band key={`band-${i}`} band={band} />
-          ))}
-      </tbody> */}
+      <tbody>{bands}</tbody>
     </table>
   );
 }
-
-const bands = props.results.map((band, i) => {
-  return (
-      <tr key={i}>
-          <th scope = "row"><img src={person.image} alt="person"/></th>
-          <td>{person.name}</td>
-          <td>{person.phone}</td>
-          <td>{person.email}</td>
-          <td>{person.dob}</td>
-
-      </tr>
-  )
-})
-return (
-  <table className="table table-striped">
-      <thead>
-          <tr>
-              <th scope="col">Image</th>
-              <th scope="col" onClick={() => props.sortBy('name')}>Name &#8597;</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Email</th>
-              <th scope="col" onClick={() => props.sortBy('age')}>D.O.B &#8597;</th>
-          </tr>
-      </thead>
-      <tbody>
-          {people}
-      </tbody>
-  </table>
-)
-}
-
-
 
 export default ResultsTable;
