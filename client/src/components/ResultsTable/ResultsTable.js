bafaf16c940dcf51165366e2fad546eafde00f72
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-05-25 12:27:08 -0500

    search updates

--- client/src/components/ResultsTable/ResultsTable.js
+++ client/src/components/ResultsTable/ResultsTable.js
@@ -1,49 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+// import API from "../utils/API";
+export default class ResultsTable extends Component {
+  state = {
+    employees: [],
+  };
+  filteredBands = [];
 
-function ResultsTable(props) {
-  return (
-    <table className="table col-10 m-auto">
-      <thead>
-        <tr>
-          <th scope="col">Band Name</th>
-          <th scope="col">City</th>
-          <th scope="col">Genre</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>The Rookies</td>
-          <td>Chicago, IL</td>
-          <td>Indie Rock</td>
-        </tr>
-      </tbody>
-      {/* <tbody>
-        {props.employees &&
-          props.employees.length &&
-          props.employees.map((employee, i) => (
-            <Employee key={`employee-${i}`} employee={employee} />
+  // componentDidMount() {
+  //   API.getBands().then((res) => {
+  //     // maps through the employee data
+  //     const bandData = res.data.results.map((band) => {
+  //       return band;
+  //     });
+  //     this.setState({ band: bandData });
+  //   });
+  // }
+
+  // componentDidUpdate() {
+  //   const filtered = this.state.bands.filter((band) => {
+  //     return band.band_name
+  //       .toLowerCase()
+  //       .includes(this.props.searchData.toLowerCase());
+  //   });
+  //   this.filteredBands = filtered;
+  // }
+
+  render() {
+    return (
+      <table className="table col-10 m-auto">
+        <thead>
+          <tr>
+            <th scope="col">Band Name</th>
+            <th scope="col">City</th>
+            <th scope="col">Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Rookies</td>
+            <td>Chicago, IL</td>
+            <td>Indie Rock</td>
+          </tr>
+        </tbody>
+        {/* <tbody>
+        {props.bands &&
+          props.bands.length &&
+          props.bands.map((band, i) => (
+            <Band key={`band-${i}`} band={band} />
           ))}
       </tbody> */}
-    </table>
-  );
+      </table>
+    );
+  }
 }
 
-// const Employee = ({ employee }) => {
-//   return (
-// <tr>
-//   <th scope="row">
-//     <img
-//       src={employee.picture.large}
-//       alt={`${employee.name.first} ${employee.name.last}`}
-//     ></img>
-//   </th>
-//   <td>{`${employee.name.first} ${employee.name.last}`}</td>
-//   <td className="text-justify">{employee.phone}</td>
-//   <td>
-//     <a href="mailto: Otto@gmail.com">{employee.email}</a>
-//   </td>
-// </tr>
-//   );
-// };
-
-export default ResultsTable;
+const Band = ({ band }) => {
+  return (
+    <tr>
+      <td>{band.band_name}</td>
+      <td>{band.location_id}</td>
+      <td>{band.genre_id}</td>
+    </tr>
+  );
+};
--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import SearchBar from "../components/Searchbar/Searchbar";
 import ResultsTable from "../components/ResultsTable/ResultsTable";
 
+
+
 function Search () {
     return(
         <div>
