f9935582a0ce1bd3dd6a008e611d2b6425745d55
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-25 13:10:15 -0400

    created login.js component

--- client/src/components/Registration/Login.js
+++ client/src/components/Registration/Login.js
@@ -1,30 +1,22 @@
-<div class="box-container">
-        <section class="single-box">
-            <h2 id="login">LOG IN</h2>
-            <form class="login-form">
-                <h3>EMAIL</h3>
-                <input id="email-login" type="EMAIL" />
-                <h3>PASSWORD</h3>
-                <input id="password-login" type="password" />
-                <div>
-                    <button class="login-form">SUBMIT</button>
-                </div>
-            </form>
-            <h4>Don't have an account? <href>Sign up here!</href>
-            </h4>
-        </section>
-        <section class="single-box">
-            <h2 id="signup">SIGN UP</h2>
-            <form class="signup-form">
-                <h3>NAME</h3>
-                <input id="name-signup" type="TEXT" />
-                <h3>EMAIL</h3>
-                <input id="email-signup" type="EMAIL" />
-                <h3>PASSWORD</h3>
-                <input id="password-signup" type="password" />
-                <div>
-                    <button type="submit">SUBMIT</button>
-                </div>
-            </form>
-        </section>
-    </div>
+import React from 'react';
+import "Registration.css"
+function Login() {
+    return (
+        <div class="box-container">
+            <section class="single-box">
+                <h2 id="login">LOG IN</h2>
+                <form class="login-form">
+                    <h3>EMAIL</h3>
+                    <input id="email-login" type="EMAIL" />
+                    <h3>PASSWORD</h3>
+                    <input id="password-login" type="password" />
+                    <div>
+                        <button class="login-form">LOGIN</button>
+                    </div>
+                </form>
+            </section>
+        </div>
+    )
+}
+
+export default Login();
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 //get request for featured band
 //get request for events based on location id
 import React from "react"
+import Login from "../components/Registration/Login"
+import Signup from "../components/Registration/Signup"
 
 function Home () {
-    return(
-    
+    return (
+    <div class="registration-container">
+        <Login />
+        <Signup />
+    </div>
     )
 }
 
