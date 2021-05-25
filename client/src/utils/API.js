5d955ffed35a0377327f9636c6ff06acacf97f9a
LS-T <leo.s.teruel@gmail.com>
2021-05-25 13:24:31 -0400

    connected client side routes to server side routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,11 +8,59 @@ const API = {
     getLocations: () => {
         return axios.get('/api/locations')
     },
+    getLocation: () => {
+        return axios.get('/api/locations/:id')
+    },
     createBand: function() {
         return axios.post('/api/bands')
     },
     postLocation: () => {
         return axios.post('/api/locations')
+    },
+    removeLocation: () => {
+        return axios.delete('/api/locations/:id')
+    },
+    updateLocation: () => {
+        return axios.put('/api/locations/:id')
+    },
+    getEvents: () => {
+        return axios.get('/api/events')
+    },
+    getSingleEvent: () => {
+        return axios.get('/api/events/:id')
+    },
+    postEvent: () => {
+        return axios.post('/api/events')
+    },
+    deleteEvent: () => {
+        return axios.delete('/api/events/:id')
+    },
+    updateEvent: () => {
+        return axios.put('/api/events/:id')
+    },
+    getBand: () => {
+        return axios.get('/api/bands/:id')
+    },
+    deleteBand: () => {
+        return axios.get('/api/bands/:id')
+    },
+    updateBand: () => {
+        return axios.put('/api/bands/:id')
+    },
+    getGenres: () => {
+        return axios.get('/api/genres')
+    },
+    getSingleGenre: () => {
+        return axios.get('/api/genres/:id')
+    },
+    createGenre: () => {
+        return axios.post('/api/genres')
+    },
+    updateGenre: () => {
+        return axios.put('/api/genres/:id')
+    },
+    deleteGenre: () => {
+        return axios.delete('/api/genres/:id')
     }
 }
 
