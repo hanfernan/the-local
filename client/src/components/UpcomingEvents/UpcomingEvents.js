d8379fcd3fe00df0e71f2e792578f80987b4972c
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-26 15:44:18 -0400

    working on rendering events on home page

---
+++ client/src/components/UpcomingEvents/UpcomingEvents.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import EventsTable from "./UpcomingEventsTable";
+import API from "../../utils/API"
+import "./UpcomingEvents.css";
+
+function UpcomingEvents() {
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        API.getEvents()
+        .then(res => {
+            console.log(res.data)
+            setEvents(res.data)
+        })
+    }, [])
+    
+    return(
+        <>
+        <EventsTable events={events}/>
+        </>
+    )
+}
+
+export default UpcomingEvents
---
+++ client/src/components/UpcomingEvents/UpcomingEventsTable.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import "./UpcomingEvents.css";
+import {Link} from "react-router-dom";
+
+function UpcomingEventsTable(props) {
+    const events = props.events.map((event, i) => {
+        console.log(event)
+        return (
+            <tr key={i}>
+                <td>
+                    <Link to={event.event_link} target="_blank" rel="noreferrer noopener">{event.event_name}</Link>
+                </td>
+                <td>{event.event_date}</td>
+                <td>{event.location_id}</td>
+                <td>
+                    <a href={`/bandpage/${event.band_id}`} >{event.band_id}</a>
+                </td>
+            </tr>
+        )
+    });
+
+    return (
+        <section>
+            <h2>Upcoming Events</h2>
+            <table
+                className="table table-hover table-condensed"
+            >
+                <thead>
+                    <tr>
+                        <th scope="col">Event Name</th>
+                        <th scope="col">Event Date</th>
+                        <th scope="col">Event Location</th>
+                        <th scope="col">Featured Band</th>
+                    </tr>
+                </thead>
+                <tbody>{events}</tbody>
+            </table>
+
+        </section>
+    );
+}
+
+export default UpcomingEventsTable
