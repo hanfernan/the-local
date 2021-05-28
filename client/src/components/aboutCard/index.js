e2abf96c34bc9de01f2816814e91eef759eb2ab5
LS-T <leo.s.teruel@gmail.com>
2021-05-28 15:26:48 -0400

    band is now updating

--- client/src/components/aboutCard/index.js
+++ client/src/components/aboutCard/index.js
@@ -1,54 +1,139 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import Row from "../Row";
 import Col from "../Col";
 import API from "../../utils/API"
 
+
 function AboutCard(props) {
+    const [location, setLocation] = useState("");
+    const [genre, setGenre] = useState("");
+    const [locationValue, setLocationValue] = useState(1);
+    const [genreValue, setGenreValue] = useState(1);
     const [state, editState] = useState(false)
-   
+    const [value, setValue] = useState({
+        band_name:'',
+        bio:'',
+        genre_id:'',
+        location_id:''
+    });
+
+    const bandRef= useRef();
+    const bioRef= useRef();
+    const genreRef="";
+    const locationRef=""
+    useEffect(() => {
+        API.getLocations().then((response) => {
+          setLocation(response.data);
+        });
+      }, []);
+
+
+    useEffect(() => {
+        API.getGenres().then((response) => {
+          setGenre(response.data);
+        });
+      }, []);
+
+    let locationsList =
+        location.length > 0 &&
+        location.map((item, i) => {
+            return <option value={item.id}>{item.location_name}</option>;
+    });
+
+    let genreList =
+    genre.length > 0 &&
+    genre.map((item, i) => {
+      return <option value={item.id}>{item.genre_name}</option>;
+    });
     
-    handleFormSubmit = (e) => {
+    const handleFormSubmit = (e) => {
         e.preventDefault();
-        API.updateBand(props.id,{body}).then(band => {
-            console.log(band)
+        API.updateBand(props.id,{band_name: value.band_name, genre_id: value.genre_id, 
+            bio: value.bio,location_id:value.location_id } ).then(res => {
+           console.log(res)
         })
         .catch((err) => {
             console.log(err);
 
-        })
+        });
 
     }
     return (
         <div className="card" style={{marginTop:50}}>
             <div class="card-header" style={{fontWeight: "bolder"}}>
+                {state ?
                 <Row>
                     <Col size="md-9">
-                        <h1 className="text-center">{props.bandName}</h1>
+                        <input placeholder={props.bandName} ref={bandRef} onChange={(e)=>setValue({...value, band_name: e.target.value})}/>
                     </Col>
                     <Col size="md-3">
-                        <button className="btn btn-dark" style={{marginLeft: 3}} onClick={() => editState(!state)}>Edit</button>
+                        <button className="btn btn-dark" style={{marginLeft: 50}} onClick={() => editState(!state)}>Edit</button>
                     </Col>
 
-                </Row>
+                </Row> :
+                <Row>
+                     <Col size="md-9">
+                         <h1 className="text-center">{props.bandName}</h1>
+                     </Col>
+                     <Col size="md-3">
+                         <button className="btn btn-dark" style={{marginLeft: 50}} onClick={() => editState(!state)}>Edit</button>
+                     </Col>
+ 
+                 </Row>}
                 
                 
             </div>
             <div class="card-body">
+                {state ?
                 <div>
-                    <p>{props.bio}</p>
-                </div>
+                    <input placeholder={props.bio} ref={bioRef} onChange={(e)=>setValue({...value, bio: e.target.value})}/>
+                </div> :
+                <div>
+                    <p>Bio: {props.bio}</p>
+                </div>}
+
                 {state ?  
                     <Row>
                     
                     <Col size="md-6">
-                        <input placeholder={props.genre}  />
+                    <div className="form-group">
+                        <label htmlFor="genre-signup">Genre</label>
+                        <select
+                            value={genreValue}
+                            onChange={((e)=>{
+                                setGenreValue(e.currentTarget.value)
+                                setValue({...value, genre_id: e.currentTarget.value})
+                            })}
+                            id="genre-signup"
+                            className="form-control"
+                            name={genreRef}
+                        >
+                            {genreList}
+                        </select>
+                        console.log({value.genre_id});
+                        console.log({genreRef})
+                    </div>
                 
                     </Col>
                     <Col size="md-6">
-                        <input placeholder={props.location} />
+                    <div className="form-group">
+                        <label htmlFor="location-signup">Location</label>
+                        <select
+                            value={locationValue}
+                            onChange={((e)=>{
+                                setLocationValue(e.currentTarget.value)
+                                setValue({...value, location_id: e.currentTarget.value})
+                            })}
+                            id="location-signup"
+                            className="form-control"
+                            name={locationRef}
+                        >
+                            {locationsList}
+                        </select>
+                    </div>   
                        
                     </Col>
-                    <button style= {{marginTop:20}}>Submit </button>
+                    <button style= {{marginTop:20}} onClick={handleFormSubmit}>Submit </button>
                 </Row> :  
                     <Row>
                     
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,75 +2,6 @@ import axios from "axios";
 
 //return axios.get so you return the promise allowing us to to do .then inside of our components
 const API = {
-<<<<<<< HEAD
-    getBands: () => {
-        return axios.get('/api/bands')
-    },
-    getLocations: () => {
-        return axios.get('/api/locations')
-    },
-    getLocation: (id) => {
-        return axios.get('/api/locations/' + id)
-    },
-    createBand: function(data) {
-        return axios.post('/api/banduser', data)
-    },
-    login: function(data) {
-        return axios.post('/api/banduser/login', data)
-    },
-    postLocation: () => {
-        return axios.post('/api/locations')
-    },
-    removeLocation: (id) => {
-        return axios.delete('/api/locations/' + id)
-    },
-    updateLocation: (id) => {
-        return axios.put('/api/locations/' + id)
-    },
-    getEvents: () => {
-        return axios.get('/api/events')
-    },
-    getSingleEvent: () => {
-        return axios.get('/api/events/:id')
-    },
-    postEvent: () => {
-        return axios.post('/api/events')
-    },
-    deleteEvent: (id) => {
-        return axios.delete('/api/events/' + id)
-    },
-    updateEvent: (id) => {
-        return axios.put('/api/events/' + id)
-    },
-    getBand: (id) => {
-        return axios.get('/api/bands/' + id)
-    },
-    deleteBand: (id) => {
-        return axios.get('/api/bands/' + id)
-    },
-    updateBand: (id,body) => {
-        return axios.put('/api/bands/' + id, body)
-    },
-    getGenres: () => {
-        return axios.get('/api/genres')
-    },
-    getSingleGenre: (id) => {
-        return axios.get('/api/genres/' + id)
-    },
-    createGenre: () => {
-        return axios.post('/api/genres')
-    },
-    updateGenre: (id) => {
-        return axios.put('/api/genres/' + id)
-    },
-    deleteGenre: (id) => {
-        return axios.delete('/api/genres/' + id)
-    },
-    getUserBand: (id) => {
-        return axios.get('/api/banduser/' + id)
-    }
-}
-=======
   getBands: () => {
     return axios.get("/api/bands");
   },
@@ -116,8 +47,8 @@ const API = {
   deleteBand: (id) => {
     return axios.get("/api/bands/" + id);
   },
-  updateBand: (id, bandUpdateData) => {
-    return axios.put("/api/bands/" + id, bandUpdateData);
+  updateBand: (id, ...bandUpdateData) => {
+    return axios.put("/api/bands/" + id, ...bandUpdateData);
   },
   getGenres: () => {
     return axios.get("/api/genres");
@@ -128,8 +59,8 @@ const API = {
   createGenre: () => {
     return axios.post("/api/genres");
   },
-  updateGenre: (id) => {
-    return axios.put("/api/genres/" + id);
+  updateGenre: (id, genreUpdate) => {
+    return axios.put("/api/genres/" + id, genreUpdate);
   },
   deleteGenre: (id) => {
     return axios.delete("/api/genres/" + id);
@@ -138,6 +69,5 @@ const API = {
     return axios.get("/api/banduser/" + id);
   },
 };
->>>>>>> 4df8fd3244047b829bd94fc31d6861d676068e52
 
 export default API;
